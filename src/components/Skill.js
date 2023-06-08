ce607d4b987c29073ab862eb71641d20ebca7377
Sammy <sammy.liaop@gmail.com>
2023-06-08 16:09:30 -0400

    css update

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -6,6 +6,66 @@ const Skill = () => {
             <div class='page'>
                 <div class='skill'>
                     <h1>Skills</h1>
+                    <div class='skill1'>
+                        <div class='skill1-img'>
+                            <img src='https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png'/>
+                            <h3>Python</h3>
+                        </div>
+                        <div class='skill1-img'>
+                            <img src='https://cdn1.iconfinder.com/data/icons/ionicons-fill-vol-2/512/logo-javascript-512.png'/>
+                            <h3>Javascript</h3>
+                        </div>
+                        <div class='skill1-img'>
+                            <img src=''/>
+                            <h3></h3>
+                        </div>
+                        <div class='skill1-img'>
+                            <img src=''/>
+                            <h3></h3>
+                        </div>
+                        <div class='skill1-img'>
+                            <img src=''/>
+                            <h3></h3>
+                        </div>
+                        <div class='skill1-img'>
+                            <img src=''/>
+                            <h3></h3>
+                        </div>
+                        <div class='skill1-img'>
+                            <img src=''/>
+                            <h3></h3>
+                        </div>
+                        <div class='skill1-img'>
+                            <img src=''/>
+                            <h3></h3>
+                        </div>
+                        <div class='skill1-img'>
+                            <img src=''/>
+                            <h3></h3>
+                        </div>
+                        <div class='skill1-img'>
+                            <img src=''/>
+                            <h3></h3>
+                        </div>
+                        <div class='skill1-img'>
+                            <img src=''/>
+                            <h3></h3>
+                        </div>
+                    </div>
+                    <div class='skill2'>
+                        <h2>Communications</h2>
+                            <div class = 'bar-border'>
+                                <div class = "bar" id = 'bar-c'>95%</div>
+                            </div>
+                        <h2>Problem Solving</h2>
+                            <div class = 'bar-border'>
+                                <div class = "bar" id = 'bar-s'>90%</div>
+                            </div>
+                        <h2>Teamwork</h2>
+                            <div class = 'bar-border'>
+                                <div class = "bar" id = 'bar-t'>100%</div>
+                            </div> 
+                    </div>
                 </div>
             </div>
         </>
