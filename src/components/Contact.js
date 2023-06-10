03333a08f5a68c70d0afe9dd33dbd003da586ac9
Sammy <sammy.liaop@gmail.com>
2023-06-10 14:51:23 -0400

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,24 @@ const Contact = () => {
             <div class='page'>
                 <div class='contact'>
                     <h1>Contact</h1>
+                    <div class='contact1'>
+                        <p>  I strive to be a better developer everyday so feel free to leave me a message of advice or comment or if you'd like to contact me about anything!
+                            <br/> 
+                        </p>
+                    </div>
+                    <div class='contact2'>
+                        <form>
+                            <label for='name1'>Name:</label><br/>
+                            <input id='name1' type='text' name='name1'/><br/><br/>
+                            <label for='comment'>Comment:</label><br/>
+                            <textarea id='comment' rows='6' cols='50' name='comment'/><br/>
+                            <input type='submit' value='submit'/>
+                        </form>
+                    </div>
+                    <div class='contact3'>
+                        <p>Email:</p>
+                        <a href='mailto: sammy.liaop@gmail.com' target='_blank'><img id='mail2' class='home-img-links' src = 'https://img.icons8.com/?size=512&id=Cjuj2uISMdQ1&format=png'/></a>
+                    </div>
                 </div>
             </div>
         </>
