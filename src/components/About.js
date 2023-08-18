d90e48701ce5cf2d531d8e5d37fdcc4782422031
Sammy <sammy.liaop@gmail.com>
2023-08-18 15:32:06 -0400

    testing

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,11 @@ import {useState, useRef, useEffect} from 'react'
 const About = (props) => {
     return (
         <>
+        <div class='page'>
+            <div class=''>
+
+            </div>
+        </div>
         </>
     )
 }
